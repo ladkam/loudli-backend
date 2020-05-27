b314fe9369f71d36fd478a157feaf9366c8cf61e
Amine <Amineladiki@gmail.com>
2020-05-27 11:07:24 +0200

    whitelist changes

--- marketplace_api/settings.py
+++ marketplace_api/settings.py
@@ -67,7 +67,7 @@ CORS_ORIGIN_WHITELIST = (
     'https://127.0.0.1:8001',
     'http://localhost:8081',
     'https://192.168.0.32:8080',
-    'http://loudli.s3-website.eu-west-3.amazonaws.com/'
+    'http://loudli.s3-website.eu-west-3.amazonaws.com'
 
 )
 
