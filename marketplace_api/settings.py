9ac779adf98d63d3b16f4b691403367540321fb9
Amine <Amineladiki@gmail.com>
2020-05-28 13:09:35 +0200

    logging

--- marketplace_api/settings.py
+++ marketplace_api/settings.py
@@ -110,6 +110,24 @@ DATABASES = {
     }
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
