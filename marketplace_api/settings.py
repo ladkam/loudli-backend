6c2e72c2a96c6e4bedf09ba0d79ec00ce6bb185e
Amine <Amineladiki@gmail.com>
2020-06-02 20:55:30 +0200

    dwld check

--- marketplace_api/settings.py
+++ marketplace_api/settings.py
@@ -132,7 +132,7 @@ LOGGING = {
         "analyzer": {"handlers": ["analyzer"], "level": "DEBUG", "propagate": True}
     },
 }
-"""
+
 
 LOGGING = {
     'version': 1,
@@ -165,7 +165,26 @@ LOGGING = {
 
     },
 }
+"""
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
 
 
 # Password validation
