3ca2c114d014ab7e84ca966c572f6b85e599cef9
Amine <Amineladiki@gmail.com>
2020-06-19 12:38:07 +0200

    users

--- data_api/serializers.py
+++ data_api/serializers.py
@@ -8,7 +8,6 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['username','first_name','last_name','email']
 
 class UserProfileInfoSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
     class Meta:
         model = UserProfileInfo
         fields = '__all__'
