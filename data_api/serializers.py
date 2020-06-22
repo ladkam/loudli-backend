890d0ea088b0084a1151141820b1e5538647cbca
Amine <Amineladiki@gmail.com>
2020-06-22 09:39:32 +0200

    Message_set Order Changed

--- data_api/serializers.py
+++ data_api/serializers.py
@@ -91,9 +91,6 @@ class CompaignSerializer(serializers.ModelSerializer):
     ##  announcer=UserSerializer()
     message_set = MessageSerializer(many=True)
 
-    def message_set_get(self, instance):
-        messages = instance.message_set.all().order_by('sendDate')
-        return MessageSerializer(messages, many=True).data
     class Meta:
         model = Compaign
         fields = ('id','name', 'message_set')
