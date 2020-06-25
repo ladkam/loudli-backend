cf81bd0aaaba3d2ae905c21cf52ffd575866b346
Amine <Amineladiki@gmail.com>
2020-06-25 14:30:05 +0200

    Com Details Changes

--- data_api/serializers.py
+++ data_api/serializers.py
@@ -94,7 +94,7 @@ class CompaignSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Compaign
-        fields = ('id','name','type','description','startDate', 'message_set','announcer','podcast','status','educationLevel','ageGroup')
+        fields = ('id','name','type','description','startDate', 'message_set','announcer','podcast','status','educationLevel','ageGroup','targetGender')
 
     def get_message_set(self, instance):
         messages = instance.message_set.all().order_by('sendDate')
