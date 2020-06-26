978ef57e7011c94508c5bc666a66d56171d63411
Amine <Amineladiki@gmail.com>
2020-06-26 20:53:39 +0200

    correct get

--- data_api/views.py
+++ data_api/views.py
@@ -133,24 +133,25 @@ class CompaignList(generics.ListCreateAPIView):
     def post(self, request, *args, **kwargs):
         if not self.request.POST._mutable:
             self.request.POST._mutable = True
+
         """
-        self.request.data.update({"educationLevel": [el for el in self.request.data['educationLevellab'].split('L')]})
-        self.request.data.update({"city": [el for el in self.request.data['citylab'].split('L')]})
-        self.request.data.update({"country": [el for el in self.request.data['countrylab'].split('L')]})
-        self.request.data.update({"interests": [el for el in self.request.data['interestslab'].split('L')]})
-        self.request.data.update({"ageGroup": [el for el in self.request.data['ageGrouplab'].split('L')]})
+                Data2=request.data
+
+        Data2.update({"educationLevel": [int(el) for el in self.request.data['educationLevellab'].split('L')]})
+        Data2.update({"city": [int(el) for el in self.request.data['citylab'].split('L')]})
+        self.request.data.update({"country": [int(el) for el in self.request.data['countrylab'].split('L')]})
+        self.request.data.update({"interests": [int(el) for el in self.request.data['interestslab'].split('L')]})
+        self.request.data.update({"ageGroup": [int(el) for el in self.request.data['ageGrouplab'].split('L')]})
         """
 
-        self.request.data.update({"educationLevel": [1, 2, 3]})
+        self.request.data.update({"educationLevel": [1,2,3]})
         self.request.data.update({"city": [5]})
-        self.request.data.update({"country": [456, 455]})
-        self.request.data.update({"interests": [578, 579]})
-        self.request.data.update({"ageGroup": [1, 2]})
+        self.request.data.update({"country": [456,455]})
+        self.request.data.update({"interests": [578,579]})
+        self.request.data.update({"ageGroup": [1,2]})
 
 
-
-
-        serializer = CompaignSerializerPost(data=    self.request.data)
+        serializer = CompaignSerializerPost(data=request.data)
         if serializer.is_valid():
             Compaign = serializer.save()
             serializer = CompaignSerializerPost(Compaign)
