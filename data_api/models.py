66b614d6f8682a3777930f51a2d5b80c97a3c5da
Amine <Amineladiki@gmail.com>
2020-06-28 13:39:23 +0200

    Default for Compaign

--- data_api/models.py
+++ data_api/models.py
@@ -106,7 +106,7 @@ class Compaign(models.Model):
     announcer = models.ForeignKey(User, on_delete=models.CASCADE)
     podcast = models.ForeignKey(Podcast,null=True,on_delete=models.CASCADE)
     description = models.TextField(blank=True,null=True)
-    status = models.CharField(max_length=20,default='Requested')
+    status = models.CharField(max_length=20,default='Créée')
     targetGender = models.ForeignKey(Gender,blank=True,null=True,on_delete=models.CASCADE)
     compaignPicture = models.ImageField(blank=True,null=True,upload_to=scramble_uploaded_filename)
     ageGroup = models.ManyToManyField(AgeGroup,blank=True)
