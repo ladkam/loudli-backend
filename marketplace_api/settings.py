2a5b091c2e68cd93443bc67a765ad53d2848afb6
Amine <Amineladiki@gmail.com>
2020-07-06 17:09:05 +0200

    exp date

--- marketplace_api/settings.py
+++ marketplace_api/settings.py
@@ -224,7 +224,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=3),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=5),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
 
