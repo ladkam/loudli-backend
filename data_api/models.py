840eabfba672063a790df2565057a1b4e99fe739
Amine <Amineladiki@gmail.com>
2020-07-07 15:18:22 +0200

    import more data

--- data_api/models.py
+++ data_api/models.py
@@ -206,8 +206,8 @@ class Podcast(models.Model):
     city = models.ManyToManyField(City, blank=True)
     country = models.ManyToManyField(Country, blank=True)
     ageInterval = ArrayField(models.IntegerField( blank=True, null=True), blank=True,null=True)
+    categories = ArrayField(models.CharField(max_length=40,blank=True, null=True), blank=True, null=True)
     editor=models.CharField(max_length=30, blank=True, null=True)
-    categories=models.CharField(max_length=30, blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     @staticmethod
     @authenticated_users
