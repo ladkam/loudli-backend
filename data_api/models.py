6808b0c20b7969b952478b99bd55f0eb4d06b34e
Amine <Amineladiki@gmail.com>
2020-07-25 11:00:07 +0200

    audio update

--- data_api/models.py
+++ data_api/models.py
@@ -289,7 +289,7 @@ class Compaign(models.Model):
     targetGender = models.ForeignKey(Gender,blank=True,null=True,on_delete=models.CASCADE)
     compaignPicture = models.ImageField(blank=True,null=True,upload_to=scramble_uploaded_filename)
     startDateValidated = models.DateField(auto_now=True)
-    actionFor = models.IntegerField(default=0,choices = [(0,0),(1,1)])
+    actionFor = models.IntegerField()
     adText =  models.TextField(max_length=2000,blank=True,null=True)
     ageGroup = models.ManyToManyField(AgeGroup,blank=True)
     city = models.ManyToManyField(City,blank=True,null=True)
