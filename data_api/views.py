d463120339fd2d7a24806d6a0df69d3c4db4e263
Amine <Amineladiki@gmail.com>
2020-07-27 10:16:07 +0200

    update

--- data_api/views.py
+++ data_api/views.py
@@ -403,7 +403,7 @@ class NextCompaignStep(APIView):
                 compaignStatus = CompaignStatus.objects.get(id=compaignStatusId + 1)
                 message = Message.objects.create(compaign=compaign,text='Pitch envoyé',sender=request.user,type='Notification')
                 setattr(compaign, 'status', compaignStatus)
-                setattr(compaign, 'actionFor', compaign.podcast.author.id)
+                setattr(compaign, 'actionFor', compaign.podcast.author)
                 message.save()
                 proposition.save()
                 compaign.save()
