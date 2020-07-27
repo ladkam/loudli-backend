ddb7d9c760f2c0cd227802a129796eb0f4d375da
Amine <Amineladiki@gmail.com>
2020-07-27 16:51:26 +0200

    update audio

--- data_api/models.py
+++ data_api/models.py
@@ -304,6 +304,8 @@ class Compaign(models.Model):
     audioFile = models.FileField(blank=True, null=True, upload_to=scramble_uploaded_audiofilename)
     audioFileName = models.CharField(blank=True, null=True, max_length=200)
     status = models.ForeignKey(CompaignStatus,on_delete=models.CASCADE,blank=True,null=True,default=1)
+    pubDate = models.CharField(max_length=40, blank=True, null=True)
+
 
     """
     @staticmethod
