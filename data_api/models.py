6140591dde1a37b8855a23628a12e158dd749e55
Amine <Amineladiki@gmail.com>
2020-11-11 12:16:53 +0100

    duration added to episodes

--- data_api/models.py
+++ data_api/models.py
@@ -287,6 +287,7 @@ class Ep(models.Model):
     podcast = models.ForeignKey(Podcast,on_delete=models.CASCADE)
     name = models.TextField(max_length=1000,null=True,blank=True)
     Date = models.TextField(max_length=1000,null=True,blank=True)
+    Durée = models.TextField(max_length=20,null=True,blank=True)
     audio = models.URLField(max_length=500)
     image = models.URLField(max_length=500)
     text = models.TextField(max_length=2000,null=True,blank=True)
