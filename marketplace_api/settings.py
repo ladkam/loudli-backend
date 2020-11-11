61a5c906206d11687046a6127f6f3d094f2fca09
Amine <Amineladiki@gmail.com>
2020-11-11 19:07:00 +0100

    disabled debug

--- marketplace_api/settings.py
+++ marketplace_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fgb+h4tvi7*s2i_l^#dhkbhl(vo@!uop0pt5-csl-5usb_-r1b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
